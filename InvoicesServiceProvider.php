5c077c75de107466c0c30ae88c0135afd4716512
Mikail <mikail10000000@hotmail.com>
2018-09-16 18:12:44 +0400

    2

--- InvoicesServiceProvider.php
+++ InvoicesServiceProvider.php
@@ -31,7 +31,7 @@ class InvoicesServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/Templates'           => resource_path('views/vendor/invoices'),
             __DIR__.'/Config/invoices.php' => config_path('invoices.php'),
-        ], 'invoices');
+        ], 'mikainvoices');
     }
 
     /**
